4d828b4e867a10f5e466ab764a5fca06f8ae40be
romain sanson <romainsanson.gm@gmail.com>
2019-05-05 16:16:19 +0200

    sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,6 +3,15 @@ class SmsController < ApplicationController
   end
 
   def create
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
 
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    @client.api.account.messages.create(
+      from: '+33644600267',
+      to: params[:sms][:phone],
+      body: params[:sms][:body]
+    )
   end
 end
